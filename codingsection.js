dee43a20fe3d1c565b5a5e221b7d821239a74c9b
Lily Halladay <lilyhalladay14@gmail.com>
2023-02-20 18:32:48 +0000

    changes

---
+++ codingsection.js
@@ -0,0 +1,144 @@
+
+
+
+$(document).ready(function() {
+  $("#cardone").css("display", "none");
+  $("#cardtwo").css("display", "none");
+  $("#cardthree").css("display", "none");
+  $("#cardfour").css("display", "none");
+  $("#cardfive").css("display", "none");
+  $("#cardsix").css("display", "none");
+  $("#cardseven").css("display", "none");
+  $("#cardeight").css("display", "none");
+});
+
+$(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 1") {
+        $("#cardone").css("display", "block");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+  
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 2") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "block");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 3") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "block");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 4") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "block");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 5") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "block");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 6") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "block");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 7") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "block");
+        $("#cardeight").css("display", "none");
+      } 
+    });
+  });
+
+  $(document).ready(function() {
+    $("#exampleFormControlSelect1").on("change", function() {
+      var selectedOption = $(this).val();
+      if (selectedOption === "Option 8") {
+        $("#cardone").css("display", "none");
+        $("#cardtwo").css("display", "none");
+        $("#cardthree").css("display", "none");
+        $("#cardfour").css("display", "none");
+        $("#cardfive").css("display", "none");
+        $("#cardsix").css("display", "none");
+        $("#cardseven").css("display", "none");
+        $("#cardeight").css("display", "block");
+      } 
+    });
+  });
+
+  
+  
